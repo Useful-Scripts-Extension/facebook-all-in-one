d1ffa8e9e05239655a9e441e0cf1a5793422297d
Đậu Khoa <khoakomlem@gmail.com>
2024-06-21 12:02:10 +0700

    test

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import './assets/style.css';
 import MyApp from './MyApp';
 
 export function Container() {
+    console.log('test');
     const darkMode = useStore(selectors.darkMode);
 
     return (
