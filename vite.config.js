4e79e0233befb35396661e3e5c2151349d0326c5
hoang.tran12 <99.hoangtran@gmail.com>
2024-07-11 01:26:30 +0700

    redirect

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "", // https://stackoverflow.com/a/69746868/23648002
-  build: {
-    outDir: "dist",
-  },
+    plugins: [react()],
+    base: '', // https://stackoverflow.com/a/69746868/23648002
+    root: './src',
+    build: {
+        outDir: 'dist',
+    },
 });
