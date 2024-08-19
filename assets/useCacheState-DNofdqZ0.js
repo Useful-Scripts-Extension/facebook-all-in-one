2fa1f7a9073deef68cdc771336e204dcf0490227
hoang.tran12 <99.hoangtran@gmail.com>
2024-08-19 14:50:06 +0700

    update dashboard

---
+++ assets/useCacheState-DNofdqZ0.js
@@ -0,0 +1,1 @@
+import{r}from"./index-iYEoUais.js";const e={};function u(t,s){const[o,n]=r.useState(()=>(e[t]===void 0&&(e[t]=s),e[t]));return r.useEffect(()=>{e[t]=o},[o]),r.useEffect(()=>{n(e[t]!==void 0?e[t]:s)},[t]),[o,n]}function a(t,s){return e[t]!==void 0?e[t]:s}function f(t,s){const o=e[t];return e[t]=s,{oldValue:o,newValue:s}}export{a as g,f as s,u};
