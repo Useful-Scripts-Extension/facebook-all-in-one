3f8c41b1bedb305a5b40d02011f7d3f3fea45d66
hoang.tran12 <99.hoangtran@gmail.com>
2024-09-30 18:01:57 +0700

    update UI/UX connect ext + dashboard

---
+++ assets/useBreakpoint-w6URs4Gd.js
@@ -0,0 +1,1 @@
+import{r as t,q as c}from"./index-q1tSS-rR.js";import{h as a}from"./index-BIHBvm97.js";function f(){const[,r]=t.useReducer(e=>e+1,0);return r}function m(){let r=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!0;const e=t.useRef({}),n=f(),s=a();return c(()=>{const u=s.subscribe(o=>{e.current=o,r&&n()});return()=>s.unsubscribe(u)},[]),e.current}export{m as a,f as u};
