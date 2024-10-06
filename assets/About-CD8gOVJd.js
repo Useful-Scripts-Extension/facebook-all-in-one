233366636bc24865c23a413117ea6cfca8242249
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-07 00:48:48 +0700

    video variants + happy theme + video downloader

--- assets/About-CD8gOVJd.js
+++
@@ -1,1 +0,0 @@
-import{r as i,av as u}from"./index-C0GZV8r4.js";import{d as s,a5 as n}from"./MyApp-5UuG5Alv.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
--- assets/VideoWithMuted-BYhYz8Pu.js
+++
@@ -1,1 +0,0 @@
-import{ap as o,aq as r,av as s}from"./index-C0GZV8r4.js";function a(t){const e=o(r.muted);return s.jsx("video",{controls:!0,autoPlay:!0,loop:!0,muted:e,...t})}export{a as V};
