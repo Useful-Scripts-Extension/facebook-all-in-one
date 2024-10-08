a939a5b58f369e2cf8e5aaf35375caf5f35e3204
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-08 14:43:53 +0700

    timeout wrapper

---
+++ assets/About-Bijf9Ntl.js
@@ -0,0 +1,1 @@
+import{r as i,av as u}from"./index-pIYkJH0y.js";import{d as s,a5 as n}from"./MyApp-B1cJNkHL.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
