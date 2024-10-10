c0e0b1e9feeb8343c8c52fa50b5c48001385def0
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-10 16:46:14 +0700

    update autoruns

--- assets/About-B669etqu.js
+++
@@ -1,1 +0,0 @@
-import{r as i,av as u}from"./index-D36d1vsd.js";import{d as s,a8 as n}from"./MyApp-DaxJ5c9U.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
