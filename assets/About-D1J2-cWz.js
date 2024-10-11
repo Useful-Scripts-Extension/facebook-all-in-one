3bc252ed05d2569cc9b95d0cfc498163efcda67d
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-11 23:50:59 +0700

    update auto run

---
+++ assets/About-D1J2-cWz.js
@@ -0,0 +1,1 @@
+import{r as i,av as u}from"./index-DNe3TpmC.js";import{d as s,aa as n}from"./MyApp-B8-XFWgd.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
