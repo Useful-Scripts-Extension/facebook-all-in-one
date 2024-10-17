f52ed19ab6cb3488347d9192eae036570f9a5cbe
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-18 00:48:06 +0700

    tip for fb

--- assets/About-CtEOFQK3.js
+++
@@ -1,1 +0,0 @@
-import{r as i,av as u}from"./index-DYv4Q6jl.js";import{e as s,ab as n}from"./MyApp-BnGnUcXW.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
